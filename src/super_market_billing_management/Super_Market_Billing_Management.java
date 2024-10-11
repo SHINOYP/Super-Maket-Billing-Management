d9e7a192379e75ce26c49a348e7aa9fcbc4d1eac
SHINOYP <shinoyshinoo@gmail.com>
2024-10-12 01:20:24 +0530

    first commit

---
+++ src/super_market_billing_management/Super_Market_Billing_Management.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package super_market_billing_management;
+import Login.LoginForm;
+/**
+ *
+ * @author LENOVO
+ */
+public class Super_Market_Billing_Management {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          LoginForm loginForm = new LoginForm();
+    loginForm.setVisible(true);
+    }
+    
+}
